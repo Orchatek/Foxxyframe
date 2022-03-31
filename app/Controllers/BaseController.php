0c2ccf2511ea513692c3e42b119df6e7df6e2035
Peterson Umoke <umoke10@hotmail.com>
2022-03-31 17:23:18 +0100

    adding view engine to the mix

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use eftec\bladeone\BladeOne;
 use Rakit\Validation\Validator;
 use Twig\Environment;
 
@@ -10,16 +11,30 @@ abstract class BaseController
     /**
      * @var Environment
      */
-    protected $twig;
+    protected Environment $twig;
 
     /**
      * @var Validator
      */
-    protected $validator;
+    protected Validator $validator;
+
+    /**
+     * @var BladeOne
+     */
+    protected BladeOne $view;
 
     public function __construct()
     {
         $this->twig = $GLOBALS['twig'];
-        $this->validator =  new Validator();
+        $this->view = $this->viewEngine();
+        $this->validator = new Validator();
+    }
+
+    private function viewEngine(): BladeOne
+    {
+        $templatePath = APP_PATH . "/app/Views";
+        $templateCache = APP_PATH . "/storage/cache";
+
+        return (new BladeOne($templatePath, $templateCache, BladeOne::MODE_DEBUG));
     }
 }
