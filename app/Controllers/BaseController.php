3f4640631dae4259ef4a432429c0e03db92c23bf
Peterson Umoke <umoke10@hotmail.com>
2022-03-31 17:26:28 +0100

    adding twig and blade view engines to the mix

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -5,6 +5,8 @@ namespace App\Controllers;
 use eftec\bladeone\BladeOne;
 use Rakit\Validation\Validator;
 use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Twig\TwigFunction;
 
 abstract class BaseController
 {
@@ -25,16 +27,48 @@ abstract class BaseController
 
     public function __construct()
     {
-        $this->twig = $GLOBALS['twig'];
-        $this->blade = $this->viewEngine();
+        $this->twig = $this->twigViewEngine();
+        $this->blade = $this->bladeViewEngine();
         $this->validator = new Validator();
     }
 
-    private function viewEngine(): BladeOne
+    /**
+     * return a blade view engine
+     * @return BladeOne
+     */
+    private function bladeViewEngine(): BladeOne
     {
         $templatePath = APP_PATH . "/app/Views";
         $templateCache = APP_PATH . "/storage/cache";
 
         return (new BladeOne($templatePath, $templateCache, BladeOne::MODE_DEBUG));
+    }
+
+    /**
+     * return a twig view engine
+     * @return Environment
+     */
+    private function twigViewEngine(): Environment
+    {
+
+        $template_path = APP_PATH . "/app/Views";
+        $template_cache_path = APP_PATH . "/storage/cache";
+
+        // twig configurations
+        $twigLoader = new FilesystemLoader($template_path);
+        $twig = new Environment($twigLoader, [
+            'cache' => $template_cache_path,
+            'always_reload' => true,
+            'debug' => true,
+        ]);
+
+        $twig->addFunction(new TwigFunction('site_url', 'site_url'));
+        $twig->addFunction(new TwigFunction('csrf_field', 'get_csrf_field'));
+        $twig->addFunction(new TwigFunction('flash', 'flash'));
+        $twig->addFunction(new TwigFunction('config', static function ($value) {
+            return APP_DETAILS[$value];
+        }));
+
+        return $twig;
     }
 }
--- bootstrap/configs.php
+++ bootstrap/configs.php
@@ -29,27 +29,3 @@ const FLASH_ERROR = 'error';
 const FLASH_WARNING = 'warning';
 const FLASH_INFO = 'info';
 const FLASH_SUCCESS = 'success';
-
-/**
- * ================+
- * Twig Configurations Configurations
- * ================+
- */
-
-const TEMPLATE_PATH = APP_PATH . "/app/Views";
-const TEMPLATE_CACHE_PATH = APP_PATH . "/storage/cache";
-
-// twig configurations
-$twigLoader = new FilesystemLoader(TEMPLATE_PATH);
-$twig = new Environment($twigLoader, [
-    'cache' => TEMPLATE_CACHE_PATH,
-    'always_reload' => true,
-    'debug' => true,
-]);
-
-$twig->addFunction(new TwigFunction('site_url', 'site_url'));
-$twig->addFunction(new TwigFunction('csrf_field', 'get_csrf_field'));
-$twig->addFunction(new TwigFunction('flash', 'flash'));
-$twig->addFunction(new TwigFunction('config', static function ($value) {
-    return APP_DETAILS[$value];
-}));
